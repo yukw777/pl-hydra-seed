aa51fa484764affdc49431f39f23e1692c471d68
Peter Yu <2057325+yukw777@users.noreply.github.com>
2021-04-30 17:13:16 -0400

    Make the repo as a Python package

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+
+def requirements():
+    with open("requirements.txt", "r") as f:
+        return f.read().splitlines()
+
+
+setup(
+    name="project",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=requirements(),
+)
---
+++ tests/test_sample_train.py
@@ -0,0 +1,9 @@
+from project.sample_train import main
+
+from hydra.experimental import initialize, compose
+
+
+def test_main():
+    with initialize(config_path="../project/conf"):
+        cfg = compose(config_name="config")
+        main(cfg)
